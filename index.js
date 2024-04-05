42aa8f99c826b882e4494474b7a204f03b81e875
B2000144 <phamhuutri101@gmail.com>
2024-04-05 13:19:09 +0700

    kết nối Mongo

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ const mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
+dotenv.config();
+//connect DATABASE
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    // Thực hiện các thao tác khác ở đây sau khi đã kết nối thành công
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
+// mongoose.connect(
+//   "mongodb+srv://phamhuutri101:Tripham1080@cluster0.ecitgup.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+//   () => {
+//     console.log("connect to mongodb");
+//   }
+// );
 
 app.use(bodyParser.json({ limit: "50mb" })); // giúp gửi dữ liệu lên dễ hơn dưới dạng json
 app.use(cors());

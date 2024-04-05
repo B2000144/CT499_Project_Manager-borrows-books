8aa670f3f2323764e931a79ffaf3c6c2d7e4cb82
B2000144 <phamhuutri101@gmail.com>
2024-04-05 11:48:54 +0700

    khởi tạo res api đầu tiên

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ var bodyParser = require("body-parser");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 
+app.use(bodyParser.json({ limit: "50mb" })); // giúp gửi dữ liệu lên dễ hơn dưới dạng json
+app.use(cors());
+app.use(morgan("common")); // khi gửi dữ liệu lên thì sẽ trả về màn hình terminals
+
+app.get("/api", (req, res) => {
+  res.status(200).json("hello");
+});
+
 app.listen(8000, () => {
   console.log("server is running on");
 });
